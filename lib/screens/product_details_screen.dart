9599d20cf495c339062f1fef80a42799faf33bde
Abidhc <abidhasan2418@gmail.com>
2023-03-07 00:16:39 +0600

    Product details slider screen ui done

--- lib/screens/product_details_screen.dart
+++ lib/screens/product_details_screen.dart
@@ -1,5 +1,6 @@
 import 'package:ecommerce_project/utilitys/widgets/inc_dce_from_filed.dart';
 import 'package:flutter/material.dart';
+import '../utilitys/Appcolors.dart';
 import '../utilitys/widgets/product_details/product_details_slider.dart';
 
 class ProductDetailsScreen extends StatefulWidget {
@@ -28,6 +29,7 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 16),
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -50,10 +52,85 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
                   children: [
                     Icon(Icons.star,color: Colors.amberAccent,),
                     Text("4.5"),
-                    TextButton(onPressed: (){}, child: Text("Reviews"))
+                    const SizedBox(width: 8,),
+                    TextButton(onPressed: (){}, child: Text("Reviews",style: TextStyle(
+                      color:  AppColors.PrimaryColor
+                    ),)),
+                    Container(
+                      padding:EdgeInsets.all(6) ,
+                      decoration: BoxDecoration(
+                        color: AppColors.PrimaryColor,
+                        borderRadius: BorderRadius.circular(8),
+                      ),
+                      child: Icon(Icons.favorite_border,color: Colors.white,size: 14,),
+                    )
 
                   ],
+                ),
+                const SizedBox(height: 16,),
+                Text("Color",style: TextStyle(
+                  color: Colors.black54,
+                  fontWeight: FontWeight.w500,
+                  fontSize: 16
                 )
+                  ,),
+                const SizedBox(height: 8,),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for( int i=0;i<5;i++)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: CircleAvatar(
+                          radius: 16,
+                          backgroundColor: AppColors.PrimaryColor,
+                          child: Icon(Icons.check),
+                      ),
+                        )
+
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 16,),
+                Text("Size",style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16
+                )
+                  ,),
+                const SizedBox(height: 8,),
+                SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  child: Row(
+                    children: [
+                      for( int i=0;i<5;i++)
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black54,width: 2),
+                              borderRadius: BorderRadius.circular(50)
+                            ),
+                            child: Text("XL"),
+                          ),
+                        )
+
+                    ],
+                  ),
+                ),
+                Text("Description",style: TextStyle(
+                    color: Colors.black54,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 16
+                )
+                  ,),
+                const SizedBox(height: 8,),
+                Text(''' 
+                The world's largest athletic apparel company, Nike is best known for its footwear, apparel, and equipment. Founded in 1964 as Blue Ribbon Sports, the company became Nike in 1971 after the Greek goddess of victory.
+                ''')
+                
               ],
             ),
           )
